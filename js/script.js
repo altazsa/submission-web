97c27dbe7be642d42b4806405141599739bd0e8d
Altarisa Bellany <altarisaby@gmail.com>
2024-02-29 22:00:13 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toogle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Click out sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
